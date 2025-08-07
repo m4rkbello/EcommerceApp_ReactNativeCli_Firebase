96164bb8983378efad4d81bfc8a507679071053d
m4rkbello <markbello10041999@gmail.com>
2025-08-07 21:43:35 +0800

    ADD TIMELINE AND SOME IMPORTANT MATTER PALDO

--- src/screens/splash/index.js
+++ src/screens/splash/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { ImageBackground, StyleSheet, Text } from 'react-native';
 
 export default function SplashScreen() {
     return (
         <ImageBackground
             source={require('../../assets/bg.jpg')}
             resizeMode="cover"
-            style={styles.background}
-        />
+            style={{ flex: 1, padding: 15 }}
+        >
+            <Text style={{ fontSize: 30, color: 'white', fontWeight: 'bold' }}>
+                E-Commerce APP
+            </Text>
+        </ImageBackground>
     );
 }
 
