958b39e3a07965efa7e466023aaa580936669d80
m4rkbello <markbello10041999@gmail.com>
2025-08-07 21:27:36 +0800

    ADD AN  IMAGE AS  BACKGROUND COVER

--- App.tsx
+++ App.tsx
@@ -1,35 +1,18 @@
 import React from 'react';
-import {
-  View,
-  SafeAreaView,
-  StyleSheet,
-  StatusBar,
-  useColorScheme,
-} from 'react-native';
-import SplashScreen from './src/screens/splash'; // ensure the file name is correct and case-sensitive
+import { SafeAreaView, StyleSheet } from 'react-native';
+import SplashScreen from './src/screens/splash';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <View style={styles.container}>
-        <SplashScreen />
-      </View>
+    <SafeAreaView style={styles.container}>
+      <SplashScreen />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: '#fff', // or '#000' if dark mode
-  },
   container: {
     flex: 1,
-    justifyContent: 'center', // optional
-    alignItems: 'center', // optional
   },
 });
 
--- src/screens/splash/index.js
+++ src/screens/splash/index.js
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { ImageBackground, StyleSheet } from 'react-native';
 
 export default function SplashScreen() {
     return (
-        <View>
-            <Text>SplashScreen</Text>
-        </View>
-    )
+        <ImageBackground
+            source={require('../../assets/bg.jpg')}
+            resizeMode="cover"
+            style={styles.background}
+        />
+    );
 }
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+    },
+});
